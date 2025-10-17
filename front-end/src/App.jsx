28b73e096964067c6d826a80336316bec19e1661
Fraol <fraolm41@gmail.com>
2025-10-17 10:31:48 +0300

    Added Axios setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import { useEffect } from "react";
+import api from "./api"
 
+function App() {
+useEffect(() => {
+  api.get("./notes")
+    .then(res => console.log(res.data))
+    .catch(Err => console.log(err.response?.status))
+
+}, []);
   return (
     <>
     <h1>Note App</h1>
+    <h2>Axios Connected</h2>
     </>
   )
 }
